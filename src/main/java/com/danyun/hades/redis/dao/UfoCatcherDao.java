046403205c0499e7be1edc6c04e07ec28c13e3d1
the.lumw@gmail.com <the.lumw@gmail.com>
2017-12-22 18:12:48 +0800

    update 2017-12-21 v6

--- src/main/java/com/danyun/hades/redis/dao/UfoCatcherDao.java
+++ src/main/java/com/danyun/hades/redis/dao/UfoCatcherDao.java
@@ -7,6 +7,8 @@ public interface UfoCatcherDao {
 
     void catcherRegist(UfoCatcherRedis catcher);
 
+    boolean isUFOCatcherRegister(String catcherId);
+
     void delete(String key);
 
     void update(UfoCatcherRedis ufoCatcher);
